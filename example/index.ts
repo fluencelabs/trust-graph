57b9e3fef061dcad6b29b9e4ca8bbab46e912e19
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2021-10-04 09:24:49 +0300

    final fixes

--- example/index.ts
+++ example/index.ts
@@ -14,7 +14,16 @@
  * limitations under the License.
  */
 
-import { get_trust_bytes, issue_trust, verify_trust, add_trust, add_root, get_weight, } from "./generated/export";
+import {
+    get_trust_bytes,
+    issue_trust,
+    verify_trust,
+    add_trust,
+    add_root,
+    get_weight,
+    timestamp_sec,
+    get_all_certs
+} from "./generated/export";
 import { Fluence, KeyPair } from "@fluencelabs/fluence";
 import { Node } from "@fluencelabs/fluence-network-environment";
 const bs58 = require('bs58');
@@ -32,37 +41,50 @@ let local: Node[] = [
     },
 ];
 
+async function add_trust_helper(node: string, issuer_kp: KeyPair, issuer_peer_id: string, issued_peer_id: string, expires_at_sec: number, issued_at_sec: number) {
+    let trust_metadata = await get_trust_bytes(node, issued_peer_id, expires_at_sec, issued_at_sec);
+    const signed_metadata = await issuer_kp.Libp2pPeerId.privKey.sign(Uint8Array.from(trust_metadata.result));
+
+    let trust = await issue_trust(node, issuer_peer_id, expires_at_sec, issued_at_sec, Array.from(signed_metadata));
+    console.log("Issued trust %s", trust.trust);
+
+    let result = await verify_trust(node, trust.trust, issuer_peer_id);
+    console.log("Verify trust result: %s", result);
+
+    let result_add = await add_trust(node, trust.trust, issuer_peer_id);
+    console.log("Add trust result: %s", result_add);
+}
+
 async function main(environment: Node[]) {
-    let builtins_keypair = await KeyPair.fromBytes(bs58.decode("5CGiJio6m76GxJ2wLj46PzSu6V7SRa5agv6meR3SJBKtvTgethRCmgBJKXWDSpSEBpgNUPd7Re5cZjF8mWW4kBfs").slice(32));
+    let sk = bs58.decode("5FwE32bDcphFzuMca7Y2qW1gdR64fTBYoRNvD4MLE1hecDGhCMQGKn8aseMr5wRo4Xo2CRFdrEAawUNLYkgQD78K").slice(0, 32); // first 32 bytes - secret key, second - public key
+    let builtins_keypair = await KeyPair.fromBytes(sk);
     await Fluence.start({ connectTo: environment[0], KeyPair: builtins_keypair});
     console.log(
         "📗 created a fluence peer %s with relay %s",
         Fluence.getStatus().peerId,
         Fluence.getStatus().relayPeerId
     );
-
+    const issued_timestamp_sec = await timestamp_sec(local[0].peerId);
+    const expires_at_sec = issued_timestamp_sec + 999999999;
     const issuer_kp = await KeyPair.fromBytes(bs58.decode("29Apzfedhw2Jxh94Jj4rNSmavQ1TkNe8ALYRA7bMegobwp423aLrURxLk32WtXgXHDqoSz7GAT9fQfoMhVd1e5Ww"));
-    console.log("Issuer peer id: %", issuer_kp.Libp2pPeerId.toB58String());
 
     let add_root_result = await add_root(local[0].peerId, local[0].peerId, 2);
     console.log("Add root weight result: %s", add_root_result);
 
-    let trust_metadata = await get_trust_bytes(local[0].peerId, local[0].peerId, 99999999999, 0);
-    const signed_metadata = await issuer_kp.Libp2pPeerId.privKey.sign(Uint8Array.from(trust_metadata.result));
-
-    let root_trust = await issue_trust(local[0].peerId, local[0].peerId, 99999999999, 0, Array.from(signed_metadata));
-    console.log("Root trust %s", root_trust.trust);
-
-    let result = await verify_trust(local[0].peerId, root_trust.trust, local[0].peerId);
-    console.log("Verify root trust result: %s", result);
-
-    let result_add = await add_trust(local[0].peerId, root_trust.trust, local[0].peerId);
-    console.log("Add root trust result: %s", result_add);
+    // add root trust
+    await add_trust_helper(local[0].peerId, issuer_kp, local[0].peerId, local[0].peerId,expires_at_sec, issued_timestamp_sec);
 
     let root_weight_result = await get_weight(local[0].peerId, local[0].peerId);
     console.log("Root weight: %s", root_weight_result);
 
-    // TODO: insert trust to local[1].peerId, get this certs, add local[1].peerId as root in local[1].peerId and insert_certificate
+    // add trust
+    await add_trust_helper(local[0].peerId, issuer_kp, local[0].peerId, local[1].peerId,expires_at_sec, issued_timestamp_sec);
+    let weight_result = await get_weight(local[0].peerId, local[1].peerId);
+    console.log("Trust weight: %s", weight_result);
+
+    let certs = await get_all_certs(local[0].peerId, local[1].peerId);
+    console.log("Certs: %s", certs);
+
     return;
 }
 
