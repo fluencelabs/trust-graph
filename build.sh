e9b8475eb7591ac9ca71336c38f9cdfb23377444
Maria Kuklina <maria@fluence.one>
2023-05-03 17:32:45 +0200

    add distro crate

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# set current working directory to script directory to run script from everywhere
+cd "$(dirname "$0")"
+
+./service/build.sh
+
+mkdir -p ../distro/trust-graph-service
+cp -v service/artifacts/trust-graph.wasm service/artifacts/sqlite3.wasm service/Config.toml ../distro/trust-graph-service/
+
+cd distro
+cargo build
---
+++ distro/src/lib.rs
@@ -0,0 +1,16 @@
+pub const TRUST_GRAPH_WASM: &'static [u8] = include_bytes!("../trust-graph-service/trust-graph.wasm");
+pub const SQLITE_WASM: &'static [u8] = include_bytes!("../trust-graph-service/sqlite3.wasm");
+pub const CONFIG: &'static [u8] = include_bytes!("../trust-graph-service/Config.toml");
+
+pub mod build_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+pub use build_info::PKG_VERSION as VERSION;
+
+pub fn modules() -> std::collections::HashMap<&'static str, &'static [u8]> {
+    maplit::hashmap! {
+        "sqlite3" => SQLITE_WASM,
+        "trust-graph" => TRUST_GRAPH_WASM,
+    }
+}
